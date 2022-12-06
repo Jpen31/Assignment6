517563bd3e908d295648da40536827bedd55ee59
Jpen <jp3171@princeton.edu>
2022-12-06 16:43:32 -0500

    all

--- createdataB.c
+++ createdataB.c
@@ -6,16 +6,14 @@
 #include <stdio.h>
 #include <string.h>
 
-enum {BUFSIZE = 48};
-
 /* Creates a file containing binary. The first 48 bytes contain a name
-followed by null bytes. Then, the file has an address. */
+followed by null bytes. Then, the file has an address. Returns 0.*/
 int main(void) {
-    int BUFSIZE1 = 48;
-    int nameLength;
+    int bufferLength = 48; /* length of array */
+    int nameLength; /* length of name in the array */
     int i;
     FILE *psFile;
-    unsigned int uiAddress; /* memory address */
+    unsigned int uiData; /* memory address */
     char name[48] = "Jacob Penstein"; 
 
     psFile = fopen("dataB", "w");
@@ -23,7 +21,7 @@ int main(void) {
     nameLength = strlen(name);
     i = 0;
     /* prints a name followed by null bytes, for a total of 48 chars */
-    while (i < BUFSIZE1) {
+    while (i < bufferLength) {
         if (i < nameLength)
             putc(name[i], psFile);
         else
@@ -32,7 +30,7 @@ int main(void) {
     }
 
     /* prints a memory address */
-    fwrite(&uiAddress, sizeof(unsigned int), 1, psFile);
+    fwrite(&uiData, sizeof(unsigned int), 1, psFile);
 
     fclose(psFile);
 
